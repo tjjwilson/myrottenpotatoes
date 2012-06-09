6426e96aadcf37247009027e1cb504ddb3d9ea90
saasbook <saasbook@saasbook.(none)>
2012-06-09 10:17:42 -0700

    Adding new db migration for the movies I added to the database

---
+++ db/migrate/20120609164532_add_my_movies.rb
@@ -0,0 +1,17 @@
+class AddMyMovies < ActiveRecord::Migration
+  MY_MOVIES = [
+    {:title => 'Secondhand Lions', :rating => 'PG', :release_date => '19-Sep-2003'},
+    {:title => 'The Gnome Mobile', :rating => 'G', :release_date => '09-Jun-1967'}
+  ]
+  def up
+    MY_MOVIES.each do |movie|
+      Movie.create!(movie)
+    end
+  end
+
+  def down
+    MY_MOVIES.each do |movie|
+      Movie.find_by_title_and_rating(movie[:title], movie[:rating]).destroy
+    end$
+  end
+end
